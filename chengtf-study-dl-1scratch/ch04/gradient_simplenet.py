fffd0ae37a87574692656648b7e5260def0f6df5
chengtf94 <chengtf94@163.com>
2026-03-22 12:24:02 +0800

    in

--- chengtf-study-dl-1scratch/ch04/gradient_simplenet.py
+++ chengtf-study-dl-1scratch/ch04/gradient_simplenet.py
@@ -1,6 +1,16 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 为了导入父目录中的文件而进行的设定
+# sys.path.append(os.pardir)  # 为了导入父目录中的文件而进行的设定
+# sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
+print("当前工作目录:", os.getcwd())
+print("sys.path 路径:", sys.path)
+
+# sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
+# 自动获取当前脚本所在目录的父目录（即项目根目录）
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, project_root)  # 插入到最前面，优先查找
+print("加入后的 sys.path:", sys.path)
+
 import numpy as np
 from common.functions import softmax, cross_entropy_error
 from common.gradient import numerical_gradient
